920733f1c63680b59455d37a312ca74824ffc155
David Svantesson <pelleitorp2@gmail.com>
2023-03-08 12:24:02 +0100

    finish whit lecture-part

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import AppClass from "./ex-1-class/AppClass";
 import AppFunction from "./ex-2-function/AppFunction";
 import AppArrowFunction from "./ex-3-arrow-function/AppArrowFunction";
+import Table from "./ex-4-Table/Table";
+import StyleExsample from "./ex-5-css/StyleExsample";
+import NameList from "./ex-6-var-function/NameList";
 
 
 
@@ -16,6 +19,9 @@ const menu = <ul>
 
 console.log('>>>> hello React!');
 
+
 //ReactDOM.render(menu, document.getElementById('root'));
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<AppArrowFunction />);
+
+const names = ["Test testsson", "Name TestList"];
+root.render(<NameList names={names}/>);
